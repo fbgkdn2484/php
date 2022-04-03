97eff46b373780a9bfd31a27d3f5100971ce1b02
fbgkdn2484 <fbgkdn@naver.com>
2022-04-04 08:56:22 +0900

    master

---
+++ forms.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm bg-light">
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="/">메인</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="/Hello.php">헬로월드</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="/forms.php">머하지?</a>
+    </li>
+  </ul>
+</nav>
+<br>
+
+<div class="container">
+  <h2>검색하기</h2>
+  <form class="form-inline" action="/action_page.php">
+    <label for="email2" class="mb-2 mr-sm-2">검색:</label>
+    <input type="text" class="form-control mb-2 mr-sm-2" id="email2" placeholder="검색어를 입력해주세요." name="email">
+    <div class="form-check mb-2 mr-sm-2">
+      <label class="form-check-label">
+        <input type="checkbox" class="form-check-input" name="remember"> Remember me
+      </label>
+    </div>    
+    <button type="submit" class="btn btn-primary mb-2">검색</button>
+  </form>
+</div>
+
+</body>
+</html>
