f33c4a14995ded6b1ff4505f8a5c5259560c106f
fbgkdn2484 <fbgkdn@naver.com>
2022-04-18 09:01:02 +0900

    commit

---
+++ menu.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-sm bg-light">
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="/">메인</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="/Hello.php">헬로월드</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="/forms.php">검색</a>
+    </li>
+  </ul>
+</nav>
